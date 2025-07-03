e99caa3b6e576f9f6caa44f3c9dab15b2973741f
nihalmore05 <nihalmore05@gmail.com>
2025-07-03 11:33:34 +0530

    Adding new Loop folder

---
+++ Java/Loops/for_loop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class for_loop {
+    public static void main(String [] args){
+       int i = 0;
+       while(i < 10){
+        System.out.println("Hello Nihal");
+        i = i + 1;
+       }
+    }
+    
+}
