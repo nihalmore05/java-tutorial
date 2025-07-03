60df3f21dcab42bd1655cca338bedbcb199308b8
nihalmore05 <nihalmore05@gmail.com>
2025-07-03 11:17:57 +0530

    Adding new folder  data type

---
+++ Java/Data_Types/Non_Primative.java
@@ -0,0 +1,17 @@
+package Data_Types;
+
+
+public class Non_Primative {
+    public static void main(String[] args){
+    // --------- String -------
+        String n = "Nihal";
+        String m = "More";
+
+        System.out.println("First Name:"  + n);
+        System.out.println("Laste Name:" + m);
+
+        // -----------  Class  -----------
+        
+    }
+    
+}
---
+++ Java/Data_Types/primitive.java
@@ -0,0 +1,20 @@
+package Data_Types;
+
+public class primitive {
+//  PRIMATIVE DATA TYPES
+  public static void main(String[] args) {
+        int a = 1;
+        byte b = 127;
+        short c = 2000;
+        float d = 14;
+        char  name = 'A';
+
+
+    
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(name);
+  }
+}
